858019d809dd334403f23952d19a66883599521e
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-07-09 14:48:33 -0500

    lab complete

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 var Dice = /** @class */ (function () {
     function Dice() {
         var _this = this;
-        this.value = Math.floor((Math.random() * 6) + 1);
+        this.value = Math.floor(Math.random() * 6 + 1);
         this.div = $("<div class=\"dice shadow\">" + this.value + "</div>");
+        this.id = Dice.counter;
         this.render();
         Dice.collection.push(this);
-        this.div.click(function () {
-            _this.value = Math.floor((Math.random() * 6) + 1);
-            _this.div.text(_this.value);
-        });
+        this.div.click(function () { return _this.reRoll(); });
         this.div.dblclick(function () {
-            Dice.collection.pop();
-            var i = Dice.collection.indexOf(_this);
+            console.log('before', Dice.collection);
+            _this.div.remove();
+            var i = _this.id;
             Dice.collection.splice(i, 1);
+            console.log('after', Dice.collection);
         });
     }
     Dice.prototype.render = function () {
         $("#dice-container").append(this.div);
+        Dice.counter++;
     };
-    // static genRoll: number = Math.floor((Math.random() * 6) + 1); 
+    Dice.prototype.reRoll = function () {
+        this.value = Math.floor(Math.random() * 6 + 1);
+        this.div.text(this.value);
+    };
     Dice.collection = [];
+    Dice.counter = 0;
     return Dice;
 }());
 $("#add-dice").click(function () { return new Dice(); });
-// reRoll() {
-//     this.value = Math.floor((Math.random() * 6) + 1);
-//     this.div.text(this.value);
-// };
+$("#roll-dice").click(function () { return Dice.collection.forEach(function (dice) { return dice.reRoll(); }); });
+$('#sum-dice').click(function () {
+    var sum = Dice.collection.reduce(function (total, dice) {
+        return total += dice.value;
+    }, 0);
+    console.log(sum);
+});
--- app.ts
+++ app.ts
@@ -1,52 +1,47 @@
 class Dice {
-
-    // static genRoll: number = Math.floor((Math.random() * 6) + 1); 
-    static collection: Dice[] = [];
+  static collection: Dice[] = [];
+  static counter: number = 0; 
+ 
 
   value: number;
   div: JQuery<HTMLDivElement>;
   text: number;
+  id: number; 
+
 
   constructor() {
-    this.value = Math.floor((Math.random() * 6) + 1);
+    this.value = Math.floor(Math.random() * 6 + 1);
     this.div = $(`<div class="dice shadow">${this.value}</div>`);
+    this.id = Dice.counter; 
     this.render();
     Dice.collection.push(this);
 
-    this.div.click(() => {
-        this.value = Math.floor((Math.random() * 6) + 1);
-        this.div.text(this.value);
-    })
+    this.div.click(() => this.reRoll());
 
     this.div.dblclick(() => {
-        Dice.collection.pop();
-        let i = Dice.collection.indexOf(this);
-        Dice.collection.splice(i, 1);
-    })
+      this.div.remove(); 
+      let i:number = this.id; 
+      Dice.collection.splice(i, 1);
+    });
   }
-
 
 
   render() {
     $("#dice-container").append(this.div);
-    
+    Dice.counter++; 
+  }
+
+  reRoll(): void {
+    this.value = Math.floor(Math.random() * 6 + 1);
+    this.div.text(this.value);
   }
 }
 
 $("#add-dice").click(() => new Dice());
-
-
-        
-        
-
-
-        
-
-
-   
-    // reRoll() {
-    //     this.value = Math.floor((Math.random() * 6) + 1);
-    //     this.div.text(this.value);
-    // };
-
-
+$("#roll-dice").click(() => Dice.collection.forEach((dice) => dice.reRoll()));
+$('#sum-dice').click(() => {
+    let sum: number = Dice.collection.reduce((total, dice) => {
+        return total += dice.value; 
+    }, 0); 
+    console.log(sum); 
+});
